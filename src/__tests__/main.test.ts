5e06c068aec385f9e7c9a1581e8d3d7c1d1217ab
jupekett <jupekett@student.jyu.fi>
2021-01-22 21:34:47 +0200

    Add more tests, and refactor to be more readable
    Tests added:
    - getCoordinates
    - distanceToStation
    
    Changed Jest function `test` for `it`, as it seems more concise and
    readable to me.

--- src/__tests__/main.test.ts
+++ src/__tests__/main.test.ts
@@ -1,19 +1,76 @@
 const main = require("../main.ts");
 
-describe("Distance function", () => {
-  test("distance should be zero between identical points", () => {
-    expect(main.distance([10,-20], [10,-20])).toBe(0);
+describe("distance", () => {
+  it("is zero between identical points", () => {
+    expect(main.distance([10, -20], [10, -20])).toBe(0);
   });
 
-  test("x-axis distance should be accurate", () => {
-    expect(main.distance([100,0], [-50,0])).toBe(150);
-  })
+  it("is accurate on x-axis", () => {
+    expect(main.distance([100, 0], [-50, 0])).toBe(150);
+  });
 
-  test("y-axis distance should be accurate", () => {
-    expect(main.distance([0, 50], [0, -100])).toBe(150);
-  })
+  it("is accurate on y-axis", () => {
+    expect(main.distance([0, 50], [0, -100])).toBe(150);
+  });
 
-  test("diagonal distance should be accurate", () => {
-    expect(main.distance([0,0], [5, 12])).toBe(13);
-  })
+  it("is accurate diagonally", () => {
+    expect(main.distance([0, 0], [5, 12])).toBe(13);
+  });
+
+  it("is accurate with floating point coordinates", () => {
+    expect(main.distance([-44.44, 33.33], [-22.22, 11.11])).toBeCloseTo(31.424);
+  });
+});
+
+describe("getCoordinates", () => {
+  it("gets coordinates", () => {
+    const stations: Station[] = [
+      [0, 0, 0],
+      [1000, -1000, 500],
+      [-12.34, 56.78, 91.23],
+    ];
+    for (let station of stations) {
+      expect(main.getCoordinates(station)).toEqual([station[0], station[1]]);
+    }
+  });
+
+  /*
+   * Notes:
+   * - should nulls and undefineds be allowed in the tuple or not?
+   * - what is to be expected with a mismatched number of parameters?
+   */
+});
+
+describe("distanceToStation", () => {
+  it(
+    "has identical return values as function 'distance' for tuples with " +
+      "identical x- and y-coordinates",
+    () => {
+      const points: Point[] = [
+        [0, 0],
+        [-999, 999],
+        [55.55, 44.44],
+      ];
+      const stations: Station[] = [
+        [0, 0, 0],
+        [1000, -1000, 500],
+        [-12.34, 56.78, 91.23],
+      ];
+      const stationCoords: Point[] = [
+        main.getCoordinates(stations[0]),
+        main.getCoordinates(stations[1]),
+        main.getCoordinates(stations[2]),
+      ];
+      for (let i = 0; i < points.length; i++) {
+        expect(main.distanceToStation(points[i], stations[i])).toBe(
+          main.distance(points[i], stationCoords[i]),
+        );
+      }
+    },
+  );
+});
+
+describe("calculatePower", () => {
+  it("should blaa", () => {
+  });
 });
